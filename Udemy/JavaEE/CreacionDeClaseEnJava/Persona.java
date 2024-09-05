9b2cd39b11a5709d280ec2bc107c218eb5305823
Blasfemia <bcarvajal84@gmail.com>
2024-09-04 22:27:26 -0400

    creacion de clase

---
+++ Udemy/JavaEE/CreacionDeClaseEnJava/Persona.java
@@ -0,0 +1,43 @@
+package Udemy.JavaEE.CreacionDeClaseEnJava;
+
+public class Persona {
+    //defino Variables
+    String Nombre;
+    String apellidoPaterno;
+    String apellidoMaterno;
+    Integer Edad;
+
+    //Defino Metodos
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(Integer edad) {
+        Edad = edad;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+}
