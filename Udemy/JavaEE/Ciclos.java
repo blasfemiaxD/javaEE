f9563686511094a18530fe924f9e36b386134e26
blasfemiaxD <bcarvajal84@gmail.com>
2024-09-23 23:39:24 -0300

    Ciclos de control

---
+++ Udemy/JavaEE/Ciclos.java
@@ -0,0 +1,31 @@
+package Udemy.JavaEE;
+
+public class Ciclos {
+    public static void main(String args[]){
+        System.out.println("Ciclos");
+
+        //While and do while
+        /*var cont = 0;
+        while(cont  < 3){
+            System.out.println("while valor cont: "+cont);
+            cont++;
+        }
+
+        do{
+            System.out.println("while valor cont: "+cont);
+            cont++;
+
+        }while(cont < 3);*/
+
+        //ciclo For
+        for(var i = 0; i < 13; i++){
+            System.out.println("valor i: "+i);
+        }
+
+
+
+
+
+
+    }
+}
